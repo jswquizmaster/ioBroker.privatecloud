26de3b06ec5acc2a2672dbc71df2b8859b110b7a
Schulte-Wülwer <Johannes.Schulte-Wuelwer@de.neusoft.com>
2020-05-25 17:00:30 +0200

    initial commit based on ioBroker.template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'privatecloud adapter settings': {
+        'en': 'Adapter settings for privatecloud',
+        'de': 'Adaptereinstellungen für privatecloud',
+        'ru': 'Настройки адаптера для privatecloud',
+        'pt': 'Configurações do adaptador para privatecloud',
+        'nl': 'Adapterinstellingen voor privatecloud',
+        'fr': "Paramètres d'adaptateur pour privatecloud",
+        'it': "Impostazioni dell'adattatore per privatecloud",
+        'es': 'Ajustes del adaptador para privatecloud',
+        'pl': 'Ustawienia adaptera dla privatecloud',
+        'zh-cn': 'privatecloud的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
