74c43c45a049cc1f86a1972da5422ab81a9e8720
Schulte-Wülwer <Johannes.Schulte-Wuelwer@de.neusoft.com>
2020-05-26 12:35:06 +0200

    Initial Version

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,16 @@ systemDictionary = {
         'pl': 'Ustawienia adaptera dla privatecloud',
         'zh-cn': 'privatecloud的适配器设置'
     },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'zh-cn': '选项1'
-    },
     'localPort': {
         'en': 'local port',
         'de': 'lokaler Port',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
+        'ru': 'local port',
+        'pt': 'local port',
+        'nl': 'local port',
+        'fr': 'local port',
+        'it': 'local port',
+        'es': 'local port',
+        'pl': 'local port',
+        'zh-cn': 'local port'
     }
 };
